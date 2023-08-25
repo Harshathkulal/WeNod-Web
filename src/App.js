25d37b1970a4f648d4ed3fea5cd9a25e36a29038
harshath <harshathmkulal@email.com>
2023-08-25 22:39:57 +0530

    Refinment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 function App() {
   return (
     <div className="App">
+       <InputGroup>
+        <Form.Control
+          placeholder="Add To-Do"
+          aria-label="Todo with Submit Edit Delete"
+        />
+        <Button variant="success">Submit</Button>
+        <Button variant="warning">Edit</Button>
+        <Button variant="danger">Delete</Button>
+
+      </InputGroup>
     
     </div>
   );
